ec5ab514c5aac5c122bfab975ce1f4a8e4274242
gyudong <121427661+gyudong0908@users.noreply.github.com>
2023-08-16 22:20:31 +0900

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
+  originWhitelist: ['http://localhost:5500'],
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
   requireHeader: ['origin', 'x-requested-with'],
